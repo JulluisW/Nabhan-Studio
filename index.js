e092f8e7da4afa94012235e184b68a900a5d40f8
Julluis Wiputra <86938766+JulluisW@users.noreply.github.com>
2021-11-18 17:02:44 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const film = {
+  "Venom: Let There Be Carnage": {
+    img: "https://media.21cineplex.com/webcontent/gallery/pictures/163307229284629_287x421.jpg",
+    bioskop: [
+      {theater: "Artha Gading XXI", jam: ["13:15", "15:25", "17:35"]}
+    ]
+  }
+}
