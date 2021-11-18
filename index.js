7bbbab5bc7ea6bc64918c35fe1eea75e188d4f20
arumdyaharum <arumdyaharum@gmail.com>
2021-11-18 19:23:32 +0700

    add data film form

--- index.js
+++ index.js
@@ -48,60 +48,114 @@ const kumpulanFilm = [
       ],
     },
   },
+  {
+    "Kobocan": {
+      img: "https://media.21cineplex.com/webcontent/gallery/pictures/163307229284629_287x421.jpg",
+      sinopsis: "Eddie (Tom Hardy) yang kini sudah berteman dengan Venom berusaha hidup normal, namun masalah datang saat ia bertemu dengan Cletus Kasady (Woody Harrelson) yang diketahui sebagai inang dari symbiote dengan sebutan Carnage.",
+      bioskop: [
+        { theater: "Mall Paris Van Surabaya", jam: ["12:15", "15:20", "17:00"] },
+        { theater: "Gren Indonesia", jam: ["13:15", "15:25", "17:35"] },
+        { theater: "Artha Moro", jam: ["15:00", "18:35"] },
+      ],
+    },
+  },
 ];
 
-let inputFilm = document.getElementById("input_film");
+const inputFilm = document.getElementById("input_film");
+const inputTheater = document.getElementById("input_theater");
+const inputJam = document.getElementById("input_jam");
+const inputDataFilm = document.getElementById("input_dataFilm");
+const bookingForm = document.getElementById("booking-form")
+
+let dataFilm = '';
+let dataTheater = '';
+let dataJam = '';
 
 function pilihFilm() {
   for (const movie of kumpulanFilm) {
     for (const judul in movie) {
-      var option = document.createElement("option");
+      const option = document.createElement("option");
       option.text = judul;
       option.value = judul;
-      // var select = document.getElementById("id-to-my-select-box");
       inputFilm.appendChild(option);
-      // let inputFilm = document.createElement("#input_film")
-      // inputFilm.appendChild(option).innerHTML = judul;
-      // if (a.options[a.selectedIndex].value == judul) {
-      //   return judul;
-      // }
     }
   }
-  // return 'test'
 }
+pilihFilm()
 
 inputFilm.addEventListener("change", function () {
-  const inputTheater = document.getElementById("input_theater");
   inputTheater.classList.remove("d-none");
-  // let dataFilm = inputFilm.options[inputFilm.selectedIndex].value;
-  // // pilihTheater(dataFilm)
-  // let teater = kumpulanFilm[dataFilm].bioskop
-  // for(const id of teater) {
-  //   var option = document.createElement("option");
-  //   option.text = teater[id].theater;
-  //   option.value = teater[id].theater;
-  //   var select = document.getElementById("input_theater");
-  //   select.appendChild(option);
-  // }
+  const dataFilm = inputFilm.options[inputFilm.selectedIndex].value;
+  pilihTheater(dataFilm);
 });
 
-function pilihTheater() {
-  let dataFilm = inputFilm.options[inputFilm.selectedIndex].value;
-  // pilihTheater(dataFilm)
-  let teater = kumpulanFilm[dataFilm].bioskop;
-  for (const id of teater) {
-    var option = document.createElement("option");
-    option.text = teater[id].theater;
-    option.value = teater[id].theater;
-    var select = document.getElementById("input_theater");
-    select.appendChild(option);
+inputTheater.addEventListener("change", function () {
+  inputJam.classList.remove("d-none");
+  const dataTheater = inputTheater.options[inputTheater.selectedIndex].value;
+  const dataFilm = inputFilm.options[inputFilm.selectedIndex].value;
+  pilihJam(dataTheater, dataFilm);
+});
+
+inputDataFilm.addEventListener("click", function () {
+  dataFilm = inputFilm.options[inputFilm.selectedIndex].value;
+  dataTheater = inputTheater.options[inputTheater.selectedIndex].value;
+  dataJam = inputJam.options[inputJam.selectedIndex].value;
+  console.log(dataFilm, dataTheater, dataJam);
+  bookingForm.classList = "d-none"
+});
+
+
+function pilihTheater (namaFilm) {
+  for(let i = 0; i < kumpulanFilm.length; i++) {
+    if(kumpulanFilm[i][namaFilm]) {
+      const teater = kumpulanFilm[i][namaFilm].bioskop
+      for(let j = 0; j < teater.length; j++) {
+        const optionTheater = document.createElement("option");
+        optionTheater.text = teater[j].theater;
+        optionTheater.value = teater[j].theater;
+        inputTheater.appendChild(optionTheater);
+      }
+    }
   }
-  // let inputFilm = document.createElement("#input_film")
-  // inputFilm.appendChild(option).innerHTML = judul;
-  // if (a.options[a.selectedIndex].value == judul) {
-  //   return judul;
-  // }
 }
 
-console.log(pilihFilm());
-// console.log(pilihTheater);
+function pilihJam (tempat, namaFilm) {
+  for(let i = 0; i < kumpulanFilm.length; i++) {
+    if(kumpulanFilm[i][namaFilm]) {
+      const teater = kumpulanFilm[i][namaFilm].bioskop
+      for(let j = 0; j < teater.length; j++) {
+        // tampilkan jam option
+        if(teater[j].theater === tempat) {
+          for(let k = 0; k < teater[j].jam.length; k++) {
+            const optionJam = document.createElement("option");
+            optionJam.text = teater[j].jam[k];
+            optionJam.value = teater[j].jam[k];
+            inputJam.appendChild(optionJam);
+          }
+        }
+      }
+    }
+  }
+}
+
+
+// function pilihTheater() {
+//   let dataFilm = inputFilm.options[inputFilm.selectedIndex].value;
+//   // pilihTheater(dataFilm)
+//   let teater = kumpulanFilm[dataFilm].bioskop;
+//   for (const id of teater) {
+//     var option = document.createElement("option");
+//     option.text = teater[id].theater;
+//     option.value = teater[id].theater;
+//     var select = document.getElementById("input_theater");
+//     select.appendChild(option);
+//   }
+//   // let inputFilm = document.createElement("#input_film")
+//   // inputFilm.appendChild(option).innerHTML = judul;
+//   // if (a.options[a.selectedIndex].value == judul) {
+//   //   return judul;
+//   // }
+// }
+
+// console.log(pilihFilm());
+// // console.log(pilihTheater);
