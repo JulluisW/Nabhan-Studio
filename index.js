ce2375c18f91190935329c12c47e76008d09e56a
JulluisW <julluisw@gmail.com>
2021-11-19 14:15:45 +0700

    Final

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const kumpulanFilm = [
   {
     "Venom: Let There Be Carnage": {
       img: "https://media.21cineplex.com/webcontent/gallery/pictures/163307229284629_287x421.jpg",
-      sinopsis: "Eddie (Tom Hardy) yang kini sudah berteman dengan Venom berusaha hidup normal, namun masalah datang saat ia bertemu dengan Cletus Kasady (Woody Harrelson) yang diketahui sebagai inang dari symbiote dengan sebutan Carnage.",
+      sinopsis: `Eddie (Tom Hardy) yang kini sudah berteman dengan Venom berusaha hidup normal, namun masalah datang saat ia bertemu dengan Cletus Kasady (Woody Harrelson) yang diketahui sebagai inang dari symbiote dengan sebutan Carnage.
+      <br> <br>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus impedit fuga corrupti repellat dolore quas animi quisquam libero voluptate recusandae quasi eius deserunt, aut enim tempora voluptatibus perspiciatis optio eveniet fugit nobis labore, nostrum, totam modi aperiam? Modi optio corporis distinctio earum numquam inventore similique magni, enim iure vitae. Dolorem?`,
       bioskop: [
         { theater: "Mall Paris Van Surabaya", jam: ["12:15", "15:20", "17:00"] },
         { theater: "Gren Indonesia", jam: ["13:15", "15:25", "17:35"] },
@@ -14,7 +16,7 @@ const kumpulanFilm = [
   {
     "Superman: The Movie": {
       img: "https://flxt.tmsimg.com/NowShowing/10286/10286_aa.jpg",
-      sinopsis: "Ilmuwan Jor-El meluncurkan bayinya Kal-El, ke Bumi. Kal tumbuh menjadi Clark Kent, memiliki kemampuan tak biasa dan kekuatan untuk menjadi Superman, yang memperjuangkan kebenaran dan keadilan.",
+      sinopsis: "Ilmuwan Jor-El meluncurkan bayinya Kal-El, ke Bumi. Kal tumbuh menjadi Clark Kent, memiliki kemampuan tak biasa dan kekuatan untuk menjadi Superman, yang memperjuangkan kebenaran dan keadilan. <br><br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus impedit fuga corrupti repellat dolore quas animi quisquam libero voluptate recusandae quasi eius deserunt, aut enim tempora voluptatibus perspiciatis optio eveniet fugit nobis labore, nostrum, totam modi aperiam? Modi optio corporis distinctio earum numquam inventore similique magni, enim iure vitae. Dolorem?",
       bioskop: [
         { theater: "Mall Paris Van Surabaya", jam: ["12:15", "15:20", "17:00"] },
         { theater: "Gren Indonesia", jam: ["13:15", "15:25", "17:35"] },
@@ -27,7 +29,7 @@ const kumpulanFilm = [
     "No Time To Die": {
       img: "https://media.21cineplex.com/webcontent/gallery/pictures/163188974336814_287x421.jpg",
       sinopsis:
-        "Sejenak menikmati kehidupannya setelah pensiun, Bond harus kembali beraksi demi menyelamatkan seorang teman. Kali ini, Bond harus menelusuri jejak seorang penjahat misterius yang telah dipersenjatai dengan teknologi baru berbahaya.",
+        "Sejenak menikmati kehidupannya setelah pensiun, Bond harus kembali beraksi demi menyelamatkan seorang teman Kali ini, Bond harus menelusuri jejak seorang penjahat misterius yang telah dipersenjatai dengan teknologi baru berbahaya.<br><br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus impedit fuga corrupti repellat dolore quas animi quisquam libero voluptate recusandae quasi eius deserunt, aut enim tempora voluptatibus perspiciatis optio eveniet fugit nobis labore, nostrum, totam modi aperiam? Modi optio corporis distinctio earum numquam inventore similique magni, enim iure vitae. Dolorem? ",
       bioskop: [
         { theater: "Mall Paris Van Surabaya", jam: ["12:15", "15:20", "17:00"] },
         { theater: "Gren Indonesia", jam: ["13:15", "15:25", "17:35"] },
@@ -39,7 +41,7 @@ const kumpulanFilm = [
   {
     "Dilan Kembali ke Bandung": {
       img: "https://ultimagz.com/wp-content/uploads/dilan-91-1.png",
-      sinopsis: "Eddie (Tom Hardy) yang kini sudah berteman dengan Venom berusaha hidup normal, namun masalah datang saat ia bertemu dengan Cletus Kasady (Woody Harrelson) yang diketahui sebagai inang dari symbiote dengan sebutan Carnage.",
+      sinopsis: "In 1990, Milea along with her parents and sister Airin moved from Jakarta to Bandung. Her father is an Army officer. On her way to the school, she met Dilan, most-known as a 'bad boy' and leader of a motorbike gang who convinced her that she will sit on his bike with him as her boyfriend one day. Dilan started to flirt with her by coming to her house, making calls from a payphone and sending her odd but romantic gifts which includes a pre-filled crossword puzzle book: so you don’t have to think about the answers. <br><br>Unfortunately, Milea already has a boyfriend named Benni back in Jakarta. Despite that, she is no longer feels comfortable with him since he is perceived as rude and foul-mouthed. With Dilan's frequent overconfident movements, which initially made Nandan – her close friend who is in love with her – uncomfortable, Milea starts to develop feelings for him.",
       bioskop: [
         { theater: "Mall Paris Van Surabaya", jam: ["12:15", "15:20", "17:00"] },
         { theater: "Gren Indonesia", jam: ["13:15", "15:25", "17:35"] },
@@ -51,7 +53,7 @@ const kumpulanFilm = [
   {
     Kobocan: {
       img: "https://s2.bukalapak.com/img/7720877431/s-330-330/Komik_Kobo_Chan_Vol10_By_Masashi_Ueda___Penerbit_Elex_Media.jpg.webp",
-      sinopsis: "Eddie (Tom Hardy) yang kini sudah berteman dengan Venom berusaha hidup normal, namun masalah datang saat ia bertemu dengan Cletus Kasady (Woody Harrelson) yang diketahui sebagai inang dari symbiote dengan sebutan Carnage.",
+      sinopsis: "Kobo-chan began publication in the newspaper Yomiuri Shimbun on April 1, 1982. Beginning in December of that year, Soyosha published the series in book form. Nippon Television began airing the Kobo-chan strip on television on September 15, 1990. The weekly anime series ran on that channel from October 19, 1992, to March 21, 1994. Yomiuri had published 6,000 Kobo-chan strips by March 1999. Soyosha published Volume 60 on October 22, 2003.Houbunsha began publishing volumes on May 6, 2004. Its most recently volume, the 40th, was published on February 7, 2018.<br><br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus impedit fuga corrupti repellat dolore quas animi quisquam libero voluptate recusandae quasi eius deserunt, aut enim tempora voluptatibus perspiciatis optio eveniet fugit nobis labore, nostrum, totam modi aperiam? Modi optio corporis distinctio earum numquam inventore similique magni, enim iure vitae. Dolorem?",
       bioskop: [
         { theater: "Mall Paris Van Surabaya", jam: ["12:15", "15:20", "17:00"] },
         { theater: "Gren Indonesia", jam: ["13:15", "15:25", "17:35"] },
@@ -73,7 +75,7 @@ const emailInput = document.getElementById("input_email");
 const submitSeat = document.getElementById("submit-final");
 const cancelSeat = document.getElementById("cancel-tiket");
 const inputPembayaran = document.getElementById("input_pembayaran");
-const kembaliBeranda = document.getElementById("output-tiket-home")
+const kembaliBeranda = document.getElementById("output-tiket-home");
 
 function pilihFilm() {
   for (const movie of kumpulanFilm) {
@@ -180,7 +182,7 @@ function tampilkanKursi() {
       const divKursi = document.getElementById(idKursiBaris);
       const buttonKolom = document.createElement("button");
       buttonKolom.classList = "kursi";
-      const namaKursiKolom = "kursi" + row[i] + j;
+      const namaKursiKolom = row[i] + j;
       buttonKolom.id = namaKursiKolom;
       buttonKolom.innerHTML = row[i] + j;
       buttonKolom.setAttribute("onclick", `fungsiKursi('${namaKursiKolom}')`);
@@ -201,12 +203,15 @@ function fungsiKursi(namaKolom) {
     totalHarga -= 50000;
     jumlahKursi--;
     delete seats[namaKolom];
-    // document.getElementById(namaKolom).classList = "kelas"
+    document.getElementById("total-harga-detail").innerText = `${jumlahKursi} x Rp 50.000`;
+    document.getElementById("total-harga-total").innerText = `Rp ${totalHarga}`;
   } else {
     document.getElementById(namaKolom).classList += " bg-light";
     totalHarga += 50000;
     jumlahKursi++;
     seats[namaKolom] = 1;
+    document.getElementById("total-harga-detail").innerText = `${jumlahKursi} x Rp 50.000`;
+    document.getElementById("total-harga-total").innerText = ` Total: Rp ${totalHarga}`;
   }
 }
 
@@ -224,11 +229,13 @@ submitSeat.addEventListener("click", function () {
     submitSeat.for = "container-output-ticket";
     document.getElementById("container-cinema").classList += " d-none";
     document.getElementById("container-output-ticket").classList.remove("d-none");
-    document.getElementById("output-tiket-movie").innerText = `Movie : ${film}`;
-    document.getElementById("output-studio-tiket").innerText = `Studio : ${theater}`;
-    for(const key in seats) {
-      document.getElementById("output-seat-tiket").innerText = `Seats : ${key}`;
+    document.getElementById("output-tiket-movie").innerText = `${film}`;
+    document.getElementById("output-studio-tiket").innerText = `Theater : ${theater}`;
+    let arrSeat = [];
+    for (const key in seats) {
+      arrSeat.push(key);
     }
+    document.getElementById("output-seat-tiket").innerText = `Seats : ${arrSeat}`;
     document.getElementById("output-time-tiket").innerText = `Time : ${jam}`;
   }
 });
@@ -238,38 +245,42 @@ cancelSeat.addEventListener("click", function () {
   localStorage.clear();
 });
 
-kembaliBeranda.addEventListener("click",function() {
-  location.reload();
-})
-
+kembaliBeranda.addEventListener("click", function () {
+  document.location.reload();
+  localStorage.clear();
+});
 
 var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
 var slideIndex = 0;
@@ -282,8 +293,10 @@ function showSlides() {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 20000); // Change image every 2 seconds
 }
 // function pilihTheater() {
